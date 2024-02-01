44fecab571db5fc4989438bb9e6722e7d3c85e87
Alexander Nusov <nusov@users.noreply.github.com>
2024-02-01 11:24:31 +0300

    add csharp example

---
+++ examples/csharp/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using MessagePack;
+using NetMQ;
+using NetMQ.Sockets;
+
+var threads = new List<Thread>();
+var threadsCount = 8;
+var batchSize = 100000;
+var count = 0;
+
+var t0 = DateTime.Now;
+
+for (int i = 0; i < threadsCount; i++)
+{
+    count += batchSize;
+    threads.Add(new Thread(() =>
+    {
+        using (var client = new RequestSocket())
+        {
+            client.Connect("tcp://192.168.0.2:8004");
+
+            for (int i = 0; i < batchSize; i++)
+            {
+                client.SendFrame(MessagePackSerializer.Serialize(new object[] { "stockmq_test", i }));
+                var status = client.ReceiveFrameString();
+                var result = client.ReceiveFrameBytes();
+                var r = MessagePackSerializer.Deserialize<int>(result);
+                if (r != i)
+                {
+                    throw new Exception("Invalid response");
+                }
+            }
+        }
+    }));
+}
+
+foreach (var thread in threads)
+{
+    thread.Start();
+}
+
+foreach (var thread in threads)
+{
+    thread.Join();
+}
+
+var t1 = DateTime.Now - t0;
+Console.WriteLine($"Requests {count}, Time {t1.TotalSeconds}, RPS {count/t1.TotalSeconds}");
