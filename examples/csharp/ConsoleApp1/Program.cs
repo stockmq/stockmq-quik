bd5a92ed6dea22056d88673afc7e60a4faa59165
Alexander Nusov <nusov@users.noreply.github.com>
2024-02-01 12:30:31 +0300

    Update csharp example

--- examples/csharp/ConsoleApp1/Program.cs
+++ examples/csharp/ConsoleApp1/Program.cs
@@ -14,17 +14,18 @@ for (int i = 0; i < threadsCount; i++)
     count += batchSize;
     threads.Add(new Thread(() =>
     {
+        Console.WriteLine("Starting Thread");
         using (var client = new RequestSocket())
         {
-            client.Connect("tcp://192.168.0.2:8004");
+            client.Connect("tcp://10.211.55.3:8004");
 
             for (int i = 0; i < batchSize; i++)
             {
                 client.SendFrame(MessagePackSerializer.Serialize(new object[] { "stockmq_test", i }));
                 var status = client.ReceiveFrameString();
                 var result = client.ReceiveFrameBytes();
-                var r = MessagePackSerializer.Deserialize<int>(result);
-                if (r != i)
+                var r = MessagePackSerializer.Deserialize<int[]>(result);
+                if (r[0] != i)
                 {
                     throw new Exception("Invalid response");
                 }
