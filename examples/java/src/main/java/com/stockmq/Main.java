874f78c1d8350f50d33de564bdd9806646ee874f
Alexander Nusov <nusov@users.noreply.github.com>
2024-02-02 18:23:56 +0300

    Update Main.java

--- examples/java/src/main/java/com/stockmq/Main.java
+++ examples/java/src/main/java/com/stockmq/Main.java
@@ -15,8 +15,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("StockMQ Java Example");
