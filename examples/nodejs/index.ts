0db40a8f8f0b79f2ee3f3941082da4705a96347e
Alexander Nusov <nusov@users.noreply.github.com>
2024-02-02 15:43:30 +0300

    add nodejs example

---
+++ examples/nodejs/index.ts
@@ -0,0 +1,19 @@
+import { Request } from 'zeromq';
+import { encode, decode } from "@msgpack/msgpack";
+
+const url = "tcp://10.211.55.3:8004";
+
+async function run() {
+  const sock = new Request();
+
+  sock.connect(url);
+  console.log(`RPC Client connected to ${url}`);
+
+  await sock.send(encode(["isConnected"]));
+  const [status, result] = await sock.receive();
+
+  console.log(`Status: ${status.toString()}`);
+  console.log(`Result: ${decode(result)}`);
+}
+
+run()
